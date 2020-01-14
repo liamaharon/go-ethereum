bdcaa1b11cde4f968cd386bd348f3723b0078ba0
Kirill <kbulgakov@exodus.io>
2020-01-14 16:19:44 +0300

    WIP: try to create state db

--- cmd/poc/main.go
+++ cmd/poc/main.go
@@ -1,17 +1,24 @@
 package main
 
 import (
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/consensus/ethash"
 	"github.com/ethereum/go-ethereum/core"
 	"github.com/ethereum/go-ethereum/core/rawdb"
+	"github.com/ethereum/go-ethereum/core/state"
 	"github.com/ethereum/go-ethereum/core/vm"
+	"github.com/ethereum/go-ethereum/ethdb"
 	"github.com/ethereum/go-ethereum/params"
 	"log"
 )
 
 func main() {
-	fetchChainConfig()
-	_ = createStateProcessor()
+	chainConfig := fetchChainConfig()
+	db := createDb()
+	bc := createBlockchain(db, chainConfig)
+	createStateProcessor(chainConfig, bc)
+
+	tryToCreateStateDb(db)
 }
 
 func fetchChainConfig() *params.ChainConfig {
@@ -20,17 +27,41 @@ func fetchChainConfig() *params.ChainConfig {
 	return config
 }
 
-func createStateProcessor() *core.StateProcessor {
-	//file := "/Users/kirill/Library/Ethereum/geth/chaindata"
-	file := "/Users/kirill/geth/data/geth/chaindata"
+func createDb() ethdb.Database {
+	//file := "/Users/kirill/geth/data/geth/chaindata"
+	var (
+		root      = "/Users/kirill/geth/data/geth/chaindata"
+		namespace = "eth/db/chaindata"
+		cache     = 2048
+		handles   = 5120
+		freezer   = root + "/ancient"
+	)
+	db, _ := rawdb.NewLevelDBDatabaseWithFreezer(root, cache, handles, freezer, namespace)
+	//db, _ := rawdb.NewLevelDBDatabase(file, 128, 128, "eth/db/chaindata/")
 
-	db, _ := rawdb.NewLevelDBDatabase(file, 128, 128, "eth/db/chaindata/")
-	chainConfig := fetchChainConfig()
+	return db
+}
 
+func createBlockchain(db ethdb.Database, chainConfig *params.ChainConfig) *core.BlockChain {
 	bc, err := core.NewBlockChain(db, nil, chainConfig, ethash.NewFaker(), vm.Config{}, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	return core.NewStateProcessor(chainConfig, bc, ethash.NewFaker())
+	return bc
+}
+
+func createStateProcessor(config *params.ChainConfig, bc *core.BlockChain) *core.StateProcessor {
+	return core.NewStateProcessor(config, bc, ethash.NewFaker())
+}
+
+func tryToCreateStateDb(ethDb ethdb.Database) *state.StateDB {
+	db := state.NewDatabase(ethDb)
+	state, err := state.New(common.HexToHash("0x6a80bf5706919c445a3ea54d0ecba776568c387c5d426762ab03fe9a172def05"), db)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return state
 }
