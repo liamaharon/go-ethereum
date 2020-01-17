226b5b5534c8d40909228b7fd77e98d8c6cc2fad
Kirill <kbulgakov@exodus.io>
2020-01-17 18:47:45 +0300

    Add `traceTx` custom method

--- cmd/poc/main.go
+++ cmd/poc/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/consensus/ethash"
 	"github.com/ethereum/go-ethereum/core"
@@ -10,6 +11,7 @@ import (
 	"github.com/ethereum/go-ethereum/core/vm"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethdb"
+	"github.com/ethereum/go-ethereum/internal/ethapi"
 	"github.com/ethereum/go-ethereum/params"
 	"log"
 	"math/big"
@@ -28,10 +30,21 @@ func main() {
 	chainConfig := fetchChainConfig()
 	db := createDb()
 	bc := createBlockchain(db, chainConfig)
-	processor := createStateProcessor(chainConfig, bc)
+	//processor := createStateProcessor(chainConfig, bc)
 
 	stateDb := tryToCreateStateDb(db)
-	processBlock(db, processor, stateDb)
+
+	message := createTransferTokensMessage()
+	block := getLatestBlock(db)
+
+	vmctx := core.NewEVMContext(message, block.Header(), bc, nil)
+
+	result, err := traceTx(message, vmctx, stateDb)
+	if err != nil {
+		panic(err)
+	}
+
+	println(result)
 }
 
 func fetchChainConfig() *params.ChainConfig {
@@ -79,24 +92,85 @@ func tryToCreateStateDb(ethDb ethdb.Database) *state.StateDB {
 	return state
 }
 
-func processBlock(db ethdb.Database, processor *core.StateProcessor, state *state.StateDB) (types.Receipts, []*types.Log, uint64) {
-	block := rawdb.ReadBlock(db, blockHash, blockNumber)
+func createTransferTokensMessage() types.Message {
 	tx := types.NewTransaction(uint64(0), testAddr, big.NewInt(0), uint64(100000), big.NewInt(0), []byte("0xa9059cbb0000000000000000000000008b24eb4e6aae906058242d83e51fb077370c472000000000000000000000000000000000000000000000000000000000000003e8"))
 	signedTx, err := types.SignTx(tx, types.HomesteadSigner{}, testKey)
-
-	txs := make([]*types.Transaction, 1)
-	txs[0] = signedTx
-
-	headers := make([]*types.Header, 1)
-	headers[0] = block.Header()
-
-	newBlock := block.WithBody(txs, headers)
-
-	receipts, logs, num, err := processor.Process(newBlock, state, vm.Config{})
 
 	if err != nil {
 		panic(err)
 	}
 
-	return receipts, logs, num
+	message, err := signedTx.AsMessage(types.HomesteadSigner{})
+	if err != nil {
+		panic(err)
+	}
+
+	return message
+}
+
+// This func uses hardcoded block number at the moment
+func getLatestBlock(db ethdb.Database) *types.Block {
+	block := rawdb.ReadBlock(db, blockHash, blockNumber)
+
+	return block
+}
+
+//func processBlock(db ethdb.Database, processor *core.StateProcessor, state *state.StateDB) (types.Receipts, []*types.Log, uint64) {
+//	block := rawdb.ReadBlock(db, blockHash, blockNumber)
+//	tx := types.NewTransaction(uint64(0), testAddr, big.NewInt(0), uint64(100000), big.NewInt(0), []byte("0xa9059cbb0000000000000000000000008b24eb4e6aae906058242d83e51fb077370c472000000000000000000000000000000000000000000000000000000000000003e8"))
+//	signedTx, err := types.SignTx(tx, types.HomesteadSigner{}, testKey)
+//
+//	txs := make([]*types.Transaction, 1)
+//	txs[0] = signedTx
+//
+//	headers := make([]*types.Header, 1)
+//	headers[0] = block.Header()
+//
+//	newBlock := block.WithBody(txs, headers)
+//
+//	receipts, logs, num, err := processor.Process(newBlock, state, vm.Config{})
+//
+//	if err != nil {
+//		panic(err)
+//	}
+//
+//	return receipts, logs, num
+//}
+
+// This is a almost 100% copy from https://github.com/ethereum/go-ethereum/blob/master/eth/api_tracer.go#L725
+// transplanted here to make It independent func not a member or `PrivateDebugAPI` class
+func traceTx(message core.Message, vmctx vm.Context, statedb *state.StateDB) (*ethapi.ExecutionResult, error) {
+	// Assemble the structured logger or the JavaScript tracer
+	var (
+		tracer vm.Tracer
+		err    error
+	)
+
+	tracer = vm.NewStructLogger(&vm.LogConfig{
+		DisableMemory:  false,
+		DisableStack:   false,
+		DisableStorage: false,
+		Debug:          false,
+		Limit:          0,
+	})
+	// Run the transaction with tracing enabled.
+	vmenv := vm.NewEVM(vmctx, statedb, fetchChainConfig(), vm.Config{Debug: true, Tracer: tracer})
+
+	ret, gas, failed, err := core.ApplyMessage(vmenv, message, new(core.GasPool).AddGas(message.Gas()))
+	if err != nil {
+		return nil, fmt.Errorf("tracing failed: %v", err)
+	}
+	// Depending on the tracer type, format and return the output
+	switch tracer := tracer.(type) {
+	case *vm.StructLogger:
+		return &ethapi.ExecutionResult{
+			Gas:         gas,
+			Failed:      failed,
+			ReturnValue: fmt.Sprintf("%x", ret),
+			StructLogs:  ethapi.FormatLogs(tracer.StructLogs()),
+		}, nil
+
+	default:
+		panic(fmt.Sprintf("bad tracer type %T", tracer))
+	}
 }
